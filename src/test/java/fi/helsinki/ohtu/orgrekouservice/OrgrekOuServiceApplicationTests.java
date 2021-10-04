9bf67abd10c34f08e065c553446a7ca6bbf1f764
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-10-04 13:53:27 +0300

    added Dockerfile and gitlab-ci build / deploy pipeline for project

--- src/test/java/fi/helsinki/ohtu/orgrekouservice/OrgrekOuServiceApplicationTests.java
+++ src/test/java/fi/helsinki/ohtu/orgrekouservice/OrgrekOuServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package fi.helsinki.ohtu.orgrekouservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrgrekOuServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+
 	}
 
 }
