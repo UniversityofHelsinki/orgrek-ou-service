e883d2b108f0a787d78847592e44ac1e6f596a90
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-10-20 16:53:30 +0300

    added url foo for tests

--- src/test/java/fi/helsinki/ohtu/orgrekouservice/OrgrekOuServiceApplicationTests.java
+++ src/test/java/fi/helsinki/ohtu/orgrekouservice/OrgrekOuServiceApplicationTests.java
@@ -2,8 +2,10 @@ package fi.helsinki.ohtu.orgrekouservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"url = foo"})
 class OrgrekOuServiceApplicationTests {
 
 	@Test
