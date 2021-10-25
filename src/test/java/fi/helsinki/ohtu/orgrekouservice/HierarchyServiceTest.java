8cba6dad9973c1ac3f9e0039128922a01e33b200
vmheikki <veli-matti.heikkila@helsinki.fi>
2021-10-25 10:29:27 +0300

    added more tests

--- src/test/java/fi/helsinki/ohtu/orgrekouservice/HierarchyServiceTest.java
+++ src/test/java/fi/helsinki/ohtu/orgrekouservice/HierarchyServiceTest.java
@@ -21,46 +21,106 @@ public class HierarchyServiceTest {
     @Autowired
     private HierarchyService hierarchyService;
 
-    List<Node> parents = new ArrayList<>();
+    List<Node> nodes = new ArrayList<>();
 
-    List<NodeWrapper> parentNodesIdsWithTypes = new ArrayList<>();
+    List<NodeWrapper> nodesIdsWithTypes = new ArrayList<>();
 
     @Test
-    public void testParentNodesWithTypesShouldReturnCorrectParentNodeAndThreeTypes() {
+    public void testNodesWithTypesShouldReturnCorrectNodeAndThreeTypes() {
 
-        Node parentNode = new Node();
-        parentNode.setId("19");
-        parentNode.setName("Uusi96");
-        parentNode.setStartDate(null);
-        parentNode.setEndDate(null);
-        parentNode.setTimestamp(null);
-        parentNode.setUnique_id(47556984);
-        parents.add(parentNode);
+        Node node = new Node();
+        node.setId("19");
+        node.setName("Uusi96");
+        node.setStartDate(null);
+        node.setEndDate(null);
+        node.setTimestamp(null);
+        node.setUnique_id(47556984);
+        nodes.add(node);
 
         NodeWrapper nodeWrapper = new NodeWrapper();
         nodeWrapper.setNodeId("19");
         nodeWrapper.setType("tutkimus");
-        parentNodesIdsWithTypes.add(nodeWrapper);
+        nodesIdsWithTypes.add(nodeWrapper);
 
         nodeWrapper = new NodeWrapper();
         nodeWrapper.setNodeId("19");
         nodeWrapper.setType("talous");
-        parentNodesIdsWithTypes.add(nodeWrapper);
+        nodesIdsWithTypes.add(nodeWrapper);
 
         nodeWrapper = new NodeWrapper();
         nodeWrapper.setNodeId("19");
         nodeWrapper.setType("opetus");
-        parentNodesIdsWithTypes.add(nodeWrapper);
+        nodesIdsWithTypes.add(nodeWrapper);
 
-        List<NodeDTO> nodeDTOList = hierarchyService.getNodesWithTypes(parents, parentNodesIdsWithTypes);
+        List<NodeDTO> nodeDTOList = hierarchyService.getNodesWithTypes(nodes, nodesIdsWithTypes);
 
-        assertEquals(parents.size(), nodeDTOList.size());
-        assertEquals(parents.get(0).getId(), nodeDTOList.get(0).getNode().getId());
+        assertEquals(nodes.size(), nodeDTOList.size());
+        assertEquals(nodes.get(0).getId(), nodeDTOList.get(0).getNode().getId());
         assertEquals(3, nodeDTOList.get(0).getHierarchies().size());
         assertEquals("tutkimus", nodeDTOList.get(0).getHierarchies().get(0));
         assertEquals("talous", nodeDTOList.get(0).getHierarchies().get(1));
         assertEquals("opetus", nodeDTOList.get(0).getHierarchies().get(2));
     }
 
+
+    @Test
+    public void testTwoNodesWithTypesShouldReturnCorrectNodesAndTypes() {
+
+        Node node = new Node();
+        node.setId("19");
+        node.setName("Uusi96");
+        node.setStartDate(null);
+        node.setEndDate(null);
+        node.setTimestamp(null);
+        node.setUnique_id(47556984);
+        nodes.add(node);
+
+        node = new Node();
+        node.setId("20");
+        node.setName("Uusi97");
+        node.setStartDate(null);
+        node.setEndDate(null);
+        node.setTimestamp(null);
+        node.setUnique_id(47556985);
+        nodes.add(node);
+
+        NodeWrapper nodeWrapper = new NodeWrapper();
+        nodeWrapper.setNodeId("19");
+        nodeWrapper.setType("tutkimus");
+        nodesIdsWithTypes.add(nodeWrapper);
+
+        nodeWrapper = new NodeWrapper();
+        nodeWrapper.setNodeId("19");
+        nodeWrapper.setType("talous");
+        nodesIdsWithTypes.add(nodeWrapper);
+
+        nodeWrapper = new NodeWrapper();
+        nodeWrapper.setNodeId("19");
+        nodeWrapper.setType("opetus");
+        nodesIdsWithTypes.add(nodeWrapper);
+
+        nodeWrapper = new NodeWrapper();
+        nodeWrapper.setNodeId("20");
+        nodeWrapper.setType("opetus");
+        nodesIdsWithTypes.add(nodeWrapper);
+
+        nodeWrapper = new NodeWrapper();
+        nodeWrapper.setNodeId("20");
+        nodeWrapper.setType("talous");
+        nodesIdsWithTypes.add(nodeWrapper);
+
+        List<NodeDTO> nodeDTOList = hierarchyService.getNodesWithTypes(nodes, nodesIdsWithTypes);
+
+        assertEquals(nodes.size(), nodeDTOList.size());
+        assertEquals(nodes.get(0).getId(), nodeDTOList.get(0).getNode().getId());
+        assertEquals(nodes.get(1).getId(), nodeDTOList.get(1).getNode().getId());
+        assertEquals(3, nodeDTOList.get(0).getHierarchies().size());
+        assertEquals("tutkimus", nodeDTOList.get(0).getHierarchies().get(0));
+        assertEquals("talous", nodeDTOList.get(0).getHierarchies().get(1));
+        assertEquals("opetus", nodeDTOList.get(0).getHierarchies().get(2));
+        assertEquals(2, nodeDTOList.get(1).getHierarchies().size());
+        assertEquals("opetus", nodeDTOList.get(1).getHierarchies().get(0));
+        assertEquals("talous", nodeDTOList.get(1).getHierarchies().get(1));
+    }
 
 }
