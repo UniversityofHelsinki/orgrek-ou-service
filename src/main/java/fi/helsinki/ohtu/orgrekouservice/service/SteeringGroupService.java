5e7e5ca822262f5b60f1e521d4ab9b2614faca60
Henriikka Palva <henriikka.palva@helsinki.fi>
2022-05-30 09:33:21 +0300

    OR-725 degree titles

--- src/main/java/fi/helsinki/ohtu/orgrekouservice/service/SteeringGroupService.java
+++ src/main/java/fi/helsinki/ohtu/orgrekouservice/service/SteeringGroupService.java
@@ -1,13 +1,14 @@
 package fi.helsinki.ohtu.orgrekouservice.service;
 
 import fi.helsinki.ohtu.orgrekouservice.domain.DegreeProgrammeDTO;
-import fi.helsinki.ohtu.orgrekouservice.domain.Node;
+import fi.helsinki.ohtu.orgrekouservice.domain.TextDTO;
 import fi.helsinki.ohtu.orgrekouservice.util.Constants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,8 +21,13 @@ public class SteeringGroupService {
 
     public List<DegreeProgrammeDTO> getSteeringGroups() {
         String url = dbUrl + Constants.STEERING_API;
-        System.out.println(url);
         ResponseEntity<DegreeProgrammeDTO[]> response = restTemplate.getForEntity(url, DegreeProgrammeDTO[].class);
+        return List.of(response.getBody());
+    }
+
+    public List<TextDTO> getDegreeTitles(){
+        String url = dbUrl + Constants.DEGREE_TITLE_API;
+        ResponseEntity<TextDTO[]> response = restTemplate.getForEntity(url, TextDTO[].class);
         return List.of(response.getBody());
     }
 }
--- src/main/java/fi/helsinki/ohtu/orgrekouservice/util/Constants.java
+++ src/main/java/fi/helsinki/ohtu/orgrekouservice/util/Constants.java
@@ -1,12 +1,13 @@
 package fi.helsinki.ohtu.orgrekouservice.util;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Constants {
     public final static String EDGE_PATH = "/api/edge";
     public static final String NODE_API_PATH = "/api/node";
+
+    public static final String STEERING_API = "/api/steering/";
+    public static final String DEGREE_TITLE_API = "/api/degreeTitles";
 
     public static final String NAME_FI = "name_fi";
     public static final String NAME_EN = "name_en";
@@ -17,5 +18,16 @@ public class Constants {
     public static final String ECONOMY_HIERARCHY = "talous";
 
     public static final List<String> MAPPED_ROLES = List.of("ROLE_ADMIN", "ROLE_WRITER", "ROLE_READER_ALL");
-    public static final String STEERING_API = "/api/steering/" ;
+
+    public static final String BACHELORS_TITLE_TEXT_KEY = "kandiohjelma-joryt";
+    public static final String MASTERS_TITLE_TEXT_KEY = "maisteriohjelma-joryt";
+    public static final String DOCTORALS_TITLE_TEXT_KEY = "tohtoriohjelma-joryt";
+
+    public static final String BACHELORS_PROGRAMMES_KEY = "kandiohjelma";
+    public static final String MASTERS_PROGRAMMES_KEY = "maisteriohjelma";
+    public static final String DOCTORALS_PROGRAMMES_KEY = "tohtoriohjelma";
+
+    public static final String LANG_CODE_FI = "fi";
+    public static final String LANG_CODE_EN = "en";
+    public static final String LANG_CODE_SV = "sv";
 }
