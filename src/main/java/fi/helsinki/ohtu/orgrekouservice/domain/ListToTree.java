4264eeb5f3c18fdb231635522d0e585ddc9d9d2d
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-07-25 08:33:17 +0300

    refactored parent child relation to its own methods

--- src/main/java/fi/helsinki/ohtu/orgrekouservice/domain/ListToTree.java
+++ src/main/java/fi/helsinki/ohtu/orgrekouservice/domain/ListToTree.java
@@ -11,30 +11,38 @@ public class ListToTree {
         Map<String, TreeNodeDTO> map = new HashMap<>();
 
         for (Pair p:pairs) {
-            TreeNodeDTO child ;
-            if (map.containsKey(p.getChildNodeId())) {
-                child = map.get(p.getChildNodeId());
-            } else {
-                child = new TreeNodeDTO();
-                map.put(p.getChildNodeId(),child);
-            }
-            child.setId(p.getChildNodeId());
-            child.setParentId(p.getParentNodeId());
-            child.setNameFi(p.getNameFi());
-            child.setNameEn(p.getNameEn());
-            child.setNameSv(p.getNameSv());
-            child.setUniqueId(p.getUniqueId());
-
-            TreeNodeDTO parent ;
-            if (map.containsKey(p.getParentNodeId())) {
-                parent = map.get(p.getParentNodeId());
-            } else {
-                parent = new TreeNodeDTO();
-                map.put(p.getParentNodeId(),parent);
-            }
-            parent.setId(p.getParentNodeId());
-            parent.addChild(child);
+            TreeNodeDTO child = getChild(map, p);
+            updateParentChildRelation(map, p, child);
         }
         return map.get("a1");
+    }
+
+    private void updateParentChildRelation(Map<String, TreeNodeDTO> map, Pair p, TreeNodeDTO child) {
+        TreeNodeDTO parent ;
+        if (map.containsKey(p.getParentNodeId())) {
+            parent = map.get(p.getParentNodeId());
+        } else {
+            parent = new TreeNodeDTO();
+            map.put(p.getParentNodeId(),parent);
+        }
+        parent.setId(p.getParentNodeId());
+        parent.addChild(child);
+    }
+
+    private TreeNodeDTO getChild(Map<String, TreeNodeDTO> map, Pair p) {
+        TreeNodeDTO child ;
+        if (map.containsKey(p.getChildNodeId())) {
+            child = map.get(p.getChildNodeId());
+        } else {
+            child = new TreeNodeDTO();
+            map.put(p.getChildNodeId(),child);
+        }
+        child.setId(p.getChildNodeId());
+        child.setParentId(p.getParentNodeId());
+        child.setNameFi(p.getNameFi());
+        child.setNameEn(p.getNameEn());
+        child.setNameSv(p.getNameSv());
+        child.setUniqueId(p.getUniqueId());
+        return child;
     }
 }
