d6187d97e5ee0f7512a0b4ec4e402cccc2d354b1
vmheikki <veli-matti.heikkila@helsinki.fi>
2023-02-23 14:06:02 +0200

    new NodeAttributeController and NodeAttributeService methods to update node name attributes

---
+++ src/main/java/fi/helsinki/ohtu/orgrekouservice/controller/NodeAttributeController.java
@@ -0,0 +1,28 @@
+package fi.helsinki.ohtu.orgrekouservice.controller;
+
+import fi.helsinki.ohtu.orgrekouservice.domain.Attribute;
+import fi.helsinki.ohtu.orgrekouservice.service.NodeAttributeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/node/attributes")
+public class NodeAttributeController {
+
+    @Autowired
+    private NodeAttributeService nodeAttributeService;
+
+    @PutMapping("/names")
+    public ResponseEntity<List<Attribute>> updateNameAttributes(@RequestBody List<Attribute> attributes) {
+        try {
+            nodeAttributeService.updateNodeNameAttributes(attributes);
+            return new ResponseEntity<>(attributes, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(attributes, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
