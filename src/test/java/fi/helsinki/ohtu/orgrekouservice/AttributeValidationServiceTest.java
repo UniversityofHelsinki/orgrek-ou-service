a451ae05c2fdb2adba886e0aad5bab3858954a91
vmheikki <veli-matti.heikkila@helsinki.fi>
2023-03-10 17:11:52 +0200

    more test to NodeAttributeValidationService.java

--- src/test/java/fi/helsinki/ohtu/orgrekouservice/AttributeValidationServiceTest.java
+++ src/test/java/fi/helsinki/ohtu/orgrekouservice/AttributeValidationServiceTest.java
@@ -1,7 +1,9 @@
 package fi.helsinki.ohtu.orgrekouservice;
 
 import fi.helsinki.ohtu.orgrekouservice.domain.Attribute;
+import fi.helsinki.ohtu.orgrekouservice.domain.AttributeValidationDTO;
 import fi.helsinki.ohtu.orgrekouservice.service.NodeAttributeValidationService;
+import fi.helsinki.ohtu.orgrekouservice.util.Constants;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -57,6 +59,42 @@ public class AttributeValidationServiceTest {
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(Arrays.asList() , response.getBody());
 
+    }
+
+    @Test
+    public void testAttributesWithInValidDatesShouldReturnNodeArrayWithStatusCode422() {
+        Date startDate = new Date(2022,1,15);
+        Date endDate = new Date(2022,1,5);
+
+        List<Attribute> attributeList = new ArrayList<>();
+        Attribute validAttribute1 = new Attribute();
+        Attribute validAttribute2 = new Attribute();
+        validAttribute1.setId(123);
+        validAttribute1.setNodeId("1234");
+        validAttribute1.setKey("moro");
+        validAttribute1.setStartDate(startDate);
+        validAttribute1.setEndDate(endDate);
+        validAttribute1.setNew(false);
+        validAttribute1.setDeleted(false);
+
+        validAttribute2.setId(123);
+        validAttribute2.setNodeId("1234");
+        validAttribute2.setKey("moro");
+        validAttribute2.setStartDate(startDate);
+        validAttribute2.setEndDate(endDate);
+        validAttribute2.setNew(false);
+        validAttribute2.setDeleted(false);
+
+        attributeList.add(validAttribute1);
+        attributeList.add(validAttribute2);
+
+        ResponseEntity response =  nodeAttributeValidationService.validateNodeAttributes(attributeList);
+
+        List<AttributeValidationDTO> result = (List<AttributeValidationDTO>) response.getBody();
+        assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, response.getStatusCode());
+        assertEquals(2, result.size());
+        assertEquals(Constants.ATTRIBUTE_DATE_VALIDATION_MESSAGE_KEY, result.get(0).getErrorMessage());
+        assertEquals(Constants.ATTRIBUTE_DATE_VALIDATION_MESSAGE_KEY, result.get(1).getErrorMessage());
     }
 
 }
