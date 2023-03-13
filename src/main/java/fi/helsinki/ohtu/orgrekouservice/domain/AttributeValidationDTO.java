c1a9a8c9e8840a42b0b6070b54ad578ee0738e97
vmheikki <veli-matti.heikkila@helsinki.fi>
2023-03-13 09:12:47 +0200

    more test to NodeAttributeValidationService.java

--- src/main/java/fi/helsinki/ohtu/orgrekouservice/domain/AttributeValidationDTO.java
+++ src/main/java/fi/helsinki/ohtu/orgrekouservice/domain/AttributeValidationDTO.java
@@ -28,4 +28,24 @@ public class AttributeValidationDTO {
     public void setErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AttributeValidationDTO that = (AttributeValidationDTO) o;
+
+        if (getId() != that.getId()) return false;
+        if (!getNodeId().equals(that.getNodeId())) return false;
+        return getErrorMessage().equals(that.getErrorMessage());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + getNodeId().hashCode();
+        result = 31 * result + getErrorMessage().hashCode();
+        return result;
+    }
 }
--- src/test/java/fi/helsinki/ohtu/orgrekouservice/AttributeValidationServiceTest.java
+++ src/test/java/fi/helsinki/ohtu/orgrekouservice/AttributeValidationServiceTest.java
@@ -67,34 +67,44 @@ public class AttributeValidationServiceTest {
         Date endDate = new Date(2022,1,5);
 
         List<Attribute> attributeList = new ArrayList<>();
-        Attribute validAttribute1 = new Attribute();
-        Attribute validAttribute2 = new Attribute();
-        validAttribute1.setId(123);
-        validAttribute1.setNodeId("1234");
-        validAttribute1.setKey("moro");
-        validAttribute1.setStartDate(startDate);
-        validAttribute1.setEndDate(endDate);
-        validAttribute1.setNew(false);
-        validAttribute1.setDeleted(false);
+        Attribute inValidAttribute1 = new Attribute();
+        Attribute inValidAttribute2 = new Attribute();
+        inValidAttribute1.setId(123);
+        inValidAttribute1.setNodeId("1234");
+        inValidAttribute1.setKey("moro");
+        inValidAttribute1.setStartDate(startDate);
+        inValidAttribute1.setEndDate(endDate);
+        inValidAttribute1.setNew(false);
+        inValidAttribute1.setDeleted(false);
 
-        validAttribute2.setId(123);
-        validAttribute2.setNodeId("1234");
-        validAttribute2.setKey("moro");
-        validAttribute2.setStartDate(startDate);
-        validAttribute2.setEndDate(endDate);
-        validAttribute2.setNew(false);
-        validAttribute2.setDeleted(false);
+        inValidAttribute2.setId(1234);
+        inValidAttribute2.setNodeId("12345");
+        inValidAttribute2.setKey("moro");
+        inValidAttribute2.setStartDate(startDate);
+        inValidAttribute2.setEndDate(endDate);
+        inValidAttribute2.setNew(false);
+        inValidAttribute2.setDeleted(false);
 
-        attributeList.add(validAttribute1);
-        attributeList.add(validAttribute2);
+        attributeList.add(inValidAttribute1);
+        attributeList.add(inValidAttribute2);
 
         ResponseEntity response =  nodeAttributeValidationService.validateNodeAttributes(attributeList);
+
+        AttributeValidationDTO expectedFirstAttributeDTO = new AttributeValidationDTO();
+        expectedFirstAttributeDTO.setId(123);
+        expectedFirstAttributeDTO.setNodeId("1234");
+        expectedFirstAttributeDTO.setErrorMessage(Constants.ATTRIBUTE_DATE_VALIDATION_MESSAGE_KEY);
+
+        AttributeValidationDTO expectedSecondAttributeDTO = new AttributeValidationDTO();
+        expectedSecondAttributeDTO.setId(1234);
+        expectedSecondAttributeDTO.setNodeId("12345");
+        expectedSecondAttributeDTO.setErrorMessage(Constants.ATTRIBUTE_DATE_VALIDATION_MESSAGE_KEY);
 
         List<AttributeValidationDTO> result = (List<AttributeValidationDTO>) response.getBody();
         assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, response.getStatusCode());
         assertEquals(2, result.size());
-        assertEquals(Constants.ATTRIBUTE_DATE_VALIDATION_MESSAGE_KEY, result.get(0).getErrorMessage());
-        assertEquals(Constants.ATTRIBUTE_DATE_VALIDATION_MESSAGE_KEY, result.get(1).getErrorMessage());
+        assertEquals(expectedFirstAttributeDTO, result.get(0));
+        assertEquals(expectedSecondAttributeDTO, result.get(1));
     }
 
 }
