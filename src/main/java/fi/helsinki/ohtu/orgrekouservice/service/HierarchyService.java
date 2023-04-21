94eb09de0d7860e9ce45d526da2c16ac15ed97b4
Antti Lankinen <antti.lankinen@helsinki.fi>
2023-04-21 07:53:37 +0300

    Pahasti kesken.

--- src/main/java/fi/helsinki/ohtu/orgrekouservice/service/HierarchyService.java
+++ src/main/java/fi/helsinki/ohtu/orgrekouservice/service/HierarchyService.java
@@ -4,7 +4,7 @@ import fi.helsinki.ohtu.orgrekouservice.domain.*;
 import fi.helsinki.ohtu.orgrekouservice.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
