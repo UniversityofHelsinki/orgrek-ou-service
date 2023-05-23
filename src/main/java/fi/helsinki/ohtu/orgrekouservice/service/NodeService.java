e3e4778c0340080a13de63acc3f35d36cd32e8ee
vmheikki <veli-matti.heikkila@helsinki.fi>
2023-05-23 10:10:25 +0300

    insertNode method

--- src/main/java/fi/helsinki/ohtu/orgrekouservice/service/NodeService.java
+++ src/main/java/fi/helsinki/ohtu/orgrekouservice/service/NodeService.java
@@ -1,5 +1,6 @@
 package fi.helsinki.ohtu.orgrekouservice.service;
 
+import fi.helsinki.ohtu.orgrekouservice.domain.NewNodeDTO;
 import fi.helsinki.ohtu.orgrekouservice.domain.Node;
 import fi.helsinki.ohtu.orgrekouservice.util.Constants;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +8,8 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Service
 public class NodeService {
@@ -46,5 +49,23 @@ public class NodeService {
         updatedNode.setStartDate(node.getStartDate());
         updatedNode.setEndDate(node.getEndDate());
         return updatedNode;
+    }
+
+    public NewNodeDTO insertNewNode(NewNodeDTO newNodeDTO) {
+        try {
+            String insertNodePropertiesUrl = dbUrl + Constants.NODE_API_PATH + "/insert";
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<Object> requestEntity = new HttpEntity(newNodeDTO, headers);
+            ResponseEntity response = restTemplate.exchange(insertNodePropertiesUrl, HttpMethod.POST,  requestEntity, NewNodeDTO.class);
+            return (NewNodeDTO) response.getBody();
+        } catch (RestClientException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public NewNodeDTO updateParentNodeId(NewNodeDTO newNodeDTO, Node parentNode) {
+        newNodeDTO.setParentNodeId(parentNode.getId());
+        return newNodeDTO;
     }
 }
