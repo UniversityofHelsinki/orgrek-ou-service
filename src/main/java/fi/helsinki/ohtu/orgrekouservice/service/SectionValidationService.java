cbd1dad83dcafc66516a6563aa72dafa2702b8f2
vmheikki <veli-matti.heikkila@helsinki.fi>
2023-06-07 09:54:00 +0300

    validation for update section attribute already found in section_attribute table

--- src/main/java/fi/helsinki/ohtu/orgrekouservice/service/SectionValidationService.java
+++ src/main/java/fi/helsinki/ohtu/orgrekouservice/service/SectionValidationService.java
@@ -20,34 +20,23 @@ public class SectionValidationService {
         List<SectionValidationDTO> errorMessages = new ArrayList<>();
         validateId(sectionAttribute, type, errorMessages);
         validateFoundAtHierarchyFilter(distinctHierarchyFilterKeys, sectionAttribute, errorMessages);
-        validateDoubleSectionAttributes(sectionAttribute, sectionAttributeList, errorMessages);
+        validateDoubleSectionAttributes(sectionAttribute, sectionAttributeList, type, errorMessages);
         if (!errorMessages.isEmpty()) {
             return new ResponseEntity<>(errorMessages, HttpStatus.UNPROCESSABLE_ENTITY);
         }
         return new ResponseEntity<>(Arrays.asList(), HttpStatus.OK);
     }
 
-    private void validateDoubleSectionAttributes(SectionAttribute sectionAttribute, List<SectionAttribute> sectionAttributeList, List<SectionValidationDTO> errorMessages)  {
+    private void validateDoubleSectionAttributes(SectionAttribute sectionAttribute, List<SectionAttribute> sectionAttributeList, String type, List<SectionValidationDTO> errorMessages)  {
         boolean found = sectionAttributeList.stream().filter(sectionAttributeEntry -> sectionAttributeEntry.equals(sectionAttribute)).findAny().isPresent();
-        if (found) {
-            SectionValidationDTO validationDTO = new SectionValidationDTO();
-            validationDTO.setId(sectionAttribute.getId());
-            validationDTO.setSection(sectionAttribute.getSection());
-            validationDTO.setAttr(sectionAttribute.getAttr());
-            validationDTO.setErrorMessage(Constants.SECTION_ATTRIBUTE_FOUND_AT_SECTION_ATTRIBUTE_TABLE);
-            errorMessages.add(validationDTO);
-            logger.error("Validation failed for section attribute: " + sectionAttribute.getAttr() + " message : " + Constants.SECTION_ATTRIBUTE_FOUND_AT_SECTION_ATTRIBUTE_TABLE);
+        if (type == Constants.NEW_SECTION_ATTRIBUTE && found) {
+            validationMessage(sectionAttribute, Constants.SECTION_ATTRIBUTE_FOUND_AT_SECTION_ATTRIBUTE_TABLE, errorMessages);
         }
     }
 
     private void validateId(SectionAttribute sectionAttribute, String type, List<SectionValidationDTO> errorMessages) {
         if (sectionAttribute.getId() == null && type.equals(Constants.UPDATE_SECTION_ATTRIBUTE)) {
-            SectionValidationDTO validationDTO = new SectionValidationDTO();
-            validationDTO.setId(sectionAttribute.getId());
-            validationDTO.setSection(sectionAttribute.getSection());
-            validationDTO.setAttr(sectionAttribute.getAttr());
-            errorMessages.add(validationDTO);
-            logger.error("Validation failed on update for section attribute: " + sectionAttribute.getAttr() + " message : " + Constants.SECTION_ATTRIBUTE_ID_IS_NULL);
+            validationMessage(sectionAttribute, Constants.SECTION_ATTRIBUTE_ID_IS_NULL, errorMessages);
         }
     }
 
@@ -55,13 +44,17 @@ public class SectionValidationService {
     private static void validateFoundAtHierarchyFilter(List<String> distinctHierarchyFilterKeys, SectionAttribute sectionAttribute, List<SectionValidationDTO> errorMessages) {
         boolean found = distinctHierarchyFilterKeys.stream().filter(key -> sectionAttribute.getAttr().equals(key)).findAny().isPresent();
         if (!found) {
-            SectionValidationDTO validationDTO = new SectionValidationDTO();
-            validationDTO.setId(sectionAttribute.getId());
-            validationDTO.setSection(sectionAttribute.getSection());
-            validationDTO.setAttr(sectionAttribute.getAttr());
-            validationDTO.setErrorMessage(Constants.SECTION_ATTRIBUTE_VALIDATION_NOT_FOUND_AT_HIERARCHY_FILTER_TABLE);
-            errorMessages.add(validationDTO);
-            logger.error("Validation failed for section attribute: " + sectionAttribute.getAttr() + " message : " + Constants.SECTION_ATTRIBUTE_VALIDATION_NOT_FOUND_AT_HIERARCHY_FILTER_TABLE);
+            validationMessage(sectionAttribute, Constants.SECTION_ATTRIBUTE_VALIDATION_NOT_FOUND_AT_HIERARCHY_FILTER_TABLE, errorMessages);
         }
+    }
+
+    private static void validationMessage(SectionAttribute sectionAttribute, String sectionAttributeFoundAtSectionAttributeTable, List<SectionValidationDTO> errorMessages) {
+        SectionValidationDTO validationDTO = new SectionValidationDTO();
+        validationDTO.setId(sectionAttribute.getId());
+        validationDTO.setSection(sectionAttribute.getSection());
+        validationDTO.setAttr(sectionAttribute.getAttr());
+        validationDTO.setErrorMessage(sectionAttributeFoundAtSectionAttributeTable);
+        errorMessages.add(validationDTO);
+        logger.error("Validation failed for section attribute: " + sectionAttribute.getAttr() + " message : " + sectionAttributeFoundAtSectionAttributeTable);
     }
 }
--- src/test/java/fi/helsinki/ohtu/orgrekouservice/SectionValidationServiceTest.java
+++ src/test/java/fi/helsinki/ohtu/orgrekouservice/SectionValidationServiceTest.java
@@ -71,5 +71,55 @@ public class SectionValidationServiceTest {
         assertEquals(Constants.SECTION_ATTRIBUTE_VALIDATION_NOT_FOUND_AT_HIERARCHY_FILTER_TABLE, result.get(0).getErrorMessage());
     }
 
+    @Test
+    public void testSectionAttributeAlreadyFoundShouldReturn422() {
+        List<String> distinctHierarchyFilterKeys = new ArrayList<>();
+        distinctHierarchyFilterKeys.add("emo_lyhenne");
+        distinctHierarchyFilterKeys.add("type");
+        distinctHierarchyFilterKeys.add("foobar");
+
+        SectionAttribute sectionAttribute = new SectionAttribute();
+        sectionAttribute.setSection("codes");
+        sectionAttribute.setAttr("foobar");
+
+        List<SectionAttribute> sectionAttributeList = new ArrayList<>();
+        SectionAttribute sectionAttribute1 = new SectionAttribute();
+        sectionAttribute1.setId(1);
+        sectionAttribute1.setSection("codes");
+        sectionAttribute1.setAttr("foobar");
+
+        sectionAttributeList.add(sectionAttribute1);
+
+        ResponseEntity response = sectionValidationService.validateSectionAttributes(distinctHierarchyFilterKeys, sectionAttribute, Constants.NEW_SECTION_ATTRIBUTE, sectionAttributeList);
+        assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, response.getStatusCode());
+
+        List<SectionValidationDTO> result = (List<SectionValidationDTO>) response.getBody();
+        assertEquals(1, result.size());
+        assertEquals(Constants.SECTION_ATTRIBUTE_FOUND_AT_SECTION_ATTRIBUTE_TABLE, result.get(0).getErrorMessage());
+    }
+
+    @Test
+    public void testOldSectionAttributeFoundAtDistinctHierarchyFilterKeysShouldReturn200() {
+        List<String> distinctHierarchyFilterKeys = new ArrayList<>();
+        distinctHierarchyFilterKeys.add("emo_lyhenne");
+        distinctHierarchyFilterKeys.add("type");
+
+        SectionAttribute sectionAttribute = new SectionAttribute();
+        sectionAttribute.setId(1);
+        sectionAttribute.setSection("codes");
+        sectionAttribute.setAttr("type");
+
+        List<SectionAttribute> sectionAttributeList = new ArrayList<>();
+        SectionAttribute sectionAttribute1 = new SectionAttribute();
+        sectionAttribute1.setId(1);
+        sectionAttribute1.setSection("codes");
+        sectionAttribute1.setAttr("emo_lyhenne");
+
+        sectionAttributeList.add(sectionAttribute1);
+
+        ResponseEntity response = sectionValidationService.validateSectionAttributes(distinctHierarchyFilterKeys, sectionAttribute, Constants.UPDATE_SECTION_ATTRIBUTE, sectionAttributeList);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
 
 }
